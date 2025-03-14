2d883248cf70bb4e0a25475392ad63c8b36b2209
shreya <shreyathakuri1372@gmail.com>
2025-03-14 10:11:21 +0545

    feat:login page

---
+++ week7/loops.js
@@ -0,0 +1,24 @@
+const users = ["Ram", "Shyam", "Hari", "Gita"];
+console.log(users[0], "User 1");
+console.log(users[1], "User 2");
+console.log(users[2], "User 3");
+
+// for(let i=0; i<){
+
+// }
+
+// for loop
+for (let i = 0; i < 4; i++) {
+  document.write(`${users[i]} <br/>`);
+  //   console.log(users[i], "user");
+}
+
+// while loop
+let j = 0;
+while (j < users.length) {
+  console.log(j, "j values");
+  j++;
+  console.log(users[j]);
+}
+
+let;
