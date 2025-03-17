75508ba9aab0c61b52edfbd53f469d6b899b1be4
shreya <shreyathakuri1372@gmail.com>
2025-03-17 10:21:01 +0545

    feat: add login functionality

---
+++ week7/login.js
@@ -0,0 +1,15 @@
+function handleLogin(event) {
+  console.log(event, "event");
+  event.preventDefault();
+  let username = document.getElementById("username").value;
+  if (username) {
+    // hide login section when user is logged in
+    document.getElementsByClassName("login-section")[0].style.display = "none";
+    //show user section
+    document.getElementsByClassName("user-section")[0].style.display = "block";
+    document.getElementById("user-name").textContent = username;
+  }
+  console.log(username, "hey i am clicked");
+}
+
+document.getElementById("login-form").addEventListener("submit", handleLogin);
